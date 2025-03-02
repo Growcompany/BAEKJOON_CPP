cfecc8fc14cf13c1abb2c2e553cdaad9d2293a03
Growcompany <66565360+Growcompany@users.noreply.github.com>
2025-03-02 21:58:16 +0900

    .
    .

--- SOLVED/2166.cpp
+++ SOLVED/2166.cpp
@@ -2,20 +2,38 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <cmath>
 using namespace std;
+
+struct dot{
+	long double x,y;
+};
+
+long double ex_result(dot first, dot second, dot thrid){
+	long double result= 0;
+	result = (second.x - first.x)*(thrid.y - first.y) - (second.y - first.y)*(thrid.x - first.x);
+	return result / 2.0;
+}
 
 int main(){
 	int n;
-	vector<pair<int,int>> dots;
+	cin>>n;
+	vector<dot> dots;
 
-	int x,y;
-	long double result;
+	long double x,y;
+	long double result = 0;
 	for(int i=0; i<n; i++){
 		cin>>x>>y;
 		dots.push_back({x,y});
 	}
 
-	cout<<result;
+	for(int i=1; i<dots.size()-1; i++){
+		result += ex_result(dots[0], dots[i], dots[i+1]);
+	}
+
+	cout << fixed;
+	cout.precision(1); 
+	cout<< fabs(result);
 
 	return 0;
 }
